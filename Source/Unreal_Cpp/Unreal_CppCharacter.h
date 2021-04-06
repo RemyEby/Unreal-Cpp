936c26ff133a7331694a87bbdb934ce453bc9b34
unknown <mcaspar@gamingcampus.local>
2021-04-06 10:32:25 +0200

    Ajouter Dossier asset

--- Source/Unreal_Cpp/Unreal_CppCharacter.h
+++ Source/Unreal_Cpp/Unreal_CppCharacter.h
@@ -19,6 +19,9 @@ class AUnreal_CppCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	UPROPERTY(EditDefaultsOnly, Category = Mesh)
+	class UParticleSystem* Particle;
+
 	/** Pawn mesh: 1st person view (arms; seen only by self) */
 	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
 	USkeletalMeshComponent* Mesh1P;
